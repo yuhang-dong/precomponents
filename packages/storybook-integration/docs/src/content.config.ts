fd2ef017e654222774037ef7c9a55f2e5f8bdad6
yuhang-dong <dong_yu_hang@126.com>
2025-07-23 20:40:34 +0800

    feat: add google analytics for github page

--- packages/storybook-integration/docs/src/content.config.ts
+++ packages/storybook-integration/docs/src/content.config.ts
@@ -36,6 +36,15 @@ export const collections = {
                     data: data,
                     rendered: {
                         html:`
+                        <!-- Google tag (gtag.js) -->
+                        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q9LMW56C6V"></script>
+                        <script>
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-Q9LMW56C6V');
+                        </script>
                         <style>
                             .doc-iframe-wrapper {
                             width: 100%;
